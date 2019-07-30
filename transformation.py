c6b4c5ad7e9f9d79015936e53bc6e74d04398d95
Bryan Linton <blinton@clarity-us.com>
2019-07-30 17:45:23 -0500

    Fixed data model by adding a few dimensions and fact tables for Genre and Collections.  Also added more to the transformation.py file

--- transformation.py
+++ transformation.py
@@ -1,18 +1,42 @@
 import zipfile
 import boto3
 import io
-import urllib2
-
+import urllib.request
+import pg8000
 
 ##Read in zip file from s3
 myurl = "https://s3-us-west-2.amazonaws.com/com.guild.us-west-2.public-data/project-data/the-movies-dataset.zip"
-with urllib2.urlopen(myurl) as url:
-    zip_file = url.open()
+# with urllib.request.urlopen(myurl) as url:
+#     zip_file = url.read()
+file_name, headers = urllib.request.urlretrieve(myurl)
 
 
+print(file_name)
 ##Unzip zip file
-with zipfile.ZipFile(zip_file, mode='r') as zipf:
+with file_name.ZipFile(zip_file, mode='r') as zipf:
     for subfile in zipf.namelist():
         print(subfile)
 
 
+
+def parse_movie():
+	##Code to ingest and transform cast data
+	print("Code to ingest and transform cast data")
+
+def ingest_cast():
+	##Code to ingest and transform cast data
+	print("Code to ingest and transform cast data")
+
+def ingest_crew():
+	##Code to ingest and transform crew data
+	print("Code to ingest and transform crew data")
+
+def write_data(table_name, data):
+	##Code to ingest and transform crew data
+	print("Code to ingest and transform crew data")
+
+
+if __name__ == '__main__':
+    movie_data = parse_movie()
+    cast_data = parse_cast()
+    crew_data = parse_crew()
